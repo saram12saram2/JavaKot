87e4e5936aa322ab9a7461b6596fa376ee1a2906
Ain <133630488+saram12saram2@users.noreply.github.com>
2024-05-18 18:29:44 +0500

    Update 509.kt

--- 509.kt
+++ 509.kt
@@ -1,17 +1,18 @@
 class Solution {
     fun fib(n: Int): Int {
-        var f0 = 0
-        var f1 = 1
-        var c : Int;
-        if(n == 0){
-            return 0
-        }else{
-            for (i in 2..n){
-                c = f0 + f1
-                f0 = f1
-                f1 = c
+        var previous = 0  
+        var current = 1   
+        var next: Int     
+
+        if (n == 0) {
+            return previous
+        } else {
+            for (i in 2..n) {
+                next = previous + current
+                previous = current
+                current = next
             }
         }
-        return f1
+        return current
     }
 }
