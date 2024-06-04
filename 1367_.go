2d4bfa34d832167ec9468b7aa89255f37177708f
Ain <133630488+saram12saram2@users.noreply.github.com>
2024-06-04 19:19:20 +0500

    Update 1367_.go
    recursion

--- 1367_.go
+++ 1367_.go
@@ -1,19 +1,18 @@
-package main
+/*package main
 
 import "fmt"
 
-// Definition for singly-linked list.
 type ListNode struct {
     Val  int
     Next *ListNode
 }
 
-// Definition for a binary tree node.
 type TreeNode struct {
     Val   int
     Left  *TreeNode
     Right *TreeNode
 }
+*/
 
 // checkPath function checks if the linked list starting from the given pointer 
 // matches a downward path in the binary tree starting from the given node.
@@ -42,6 +41,7 @@ func isSubPath(head *ListNode, root *TreeNode) bool {
     return checkPath(head, root) || isSubPath(head, root.Left) || isSubPath(head, root.Right)
 }
 
+/*
 func main() {
     // Creating a sample linked list: 4 -> 2 -> 8
     head := &ListNode{4, &ListNode{2, &ListNode{8, nil}}}
@@ -65,3 +65,4 @@ func main() {
     result := isSubPath(head, root)
     fmt.Println(result) // Output: true
 }
+*/
